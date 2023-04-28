e5f78e234892dd3464d20c5f8de176ed16e1c6f4
Artem Popov <Top-axel@yandex.ru>
2023-04-29 00:50:26 +0300

    пр4.4

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
+//кнопки
 const buttonOpenProfile = document.querySelector('.profile__edit-button');
 const buttonCloseProfile = document.querySelector('.popup__close-icon');
 const buttonSubmitProfile = document.querySelector('.form__submit-button');
+//попап
 const popupProfile = document.querySelector('.popup');
+//инпуты
 const profileTitle = document.querySelector('.profile__title')
 const profileSubtitle = document.querySelector('.profile__subtitle')
 const popupName = popupProfile.querySelector('#user-name')
 const popupProfessional = popupProfile.querySelector('#user-job')
-
+//функции закрытие/открытие попапа, редактирования иппутов
 function openPopupProfile() {
     popupProfile.classList.add('popup_opened');
     popupName.value = profileTitle.textContent
@@ -23,7 +26,7 @@ function saveProfileData(e) {
     profileSubtitle.textContent = popupProfessional.value
     closePopupProfile()
 }
-
+//слушатели
 buttonSubmitProfile.addEventListener('click', saveProfileData);
 buttonOpenProfile.addEventListener('click', openPopupProfile);
 buttonCloseProfile.addEventListener('click', closePopupProfile);
